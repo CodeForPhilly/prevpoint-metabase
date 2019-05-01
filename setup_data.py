b2532021d8f4a4f758b3ac8d9c162832e6472471
swearingbird <erich.prantlbartlett@gmail.com>
2019-04-30 20:10:15 -0400

    Added auto cast to date

--- setup_data.py
+++ setup_data.py
@@ -6,4 +6,8 @@ engine = create_engine("postgresql://metabase:metabase@localhost:5433/metabase")
 
 # insert data (note: schema will be largely string columns)
 data = pd.read_csv("./hrsc_fake.csv")
+data = data.apply(lambda col: pd.to_datetime(col, errors='ignore')
+              if col.dtypes == object
+              else col,
+              axis=0)
 data.to_sql("prevpoint", engine, if_exists = "replace")
